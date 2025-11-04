d7f35a8b8ee383ee065d9487e03010b9ef11efb3
elinmolindr <elinmolinder1@gmail.com>
2025-11-04 09:25:55 +0100

    created menu

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-const toggleBtn = document.querySelector('.header__toggle');
-const nav = document.querySelector('.header__nav');
+
+//skapa toggle-knapp
+const header = document.querySelector('.header');
+const toggleBtn= document.createElement('button');
+toggleBtn.className= 'header__toggle';
+
+for (let i=0; i<3; i++){
+  toggleBtn.appendChild(document.createElement('span'));
+}
+
+const nav = document.createElement('nav');
+nav.className = 'header__nav';
+
+header.appendChild(toggleBtn);
+header.appendChild(nav);
 
 toggleBtn.addEventListener('click', () => {
   toggleBtn.classList.toggle('open');
-  nav.classList.toggle('open');
-});
+  nav.classList.toggle('open')});
+
+
+  const headerMenu = document.createElement('ul');
+  headerMenu.className ='header__menu';
+
+  ['Play online', 'Play on-site', 'The story', 'Contact us'].forEach (text=> {const li =document.createElement('li');
+    li.className='header__item';
+    const a = document.createElement('a');
+    a.className = 'header__link';
+    a.href = '#';
+    a.textContent =text;
+    li.appendChild(a);
+    headerMenu.appendChild(li);
+  });
+  nav.appendChild(headerMenu);
