01e51066de621a1ea88c9b3be286892c6c41f9f1
elinmolindr <elinmolinder1@gmail.com>
2025-11-04 10:53:52 +0100

    code for toggle button

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
 //skapa toggle-knapp
 const header = document.querySelector('.header');
-const toggleBtn= document.createElement('button');
-toggleBtn.className= 'header__toggle';
+const toggleBtn = document.createElement('button');
+toggleBtn.className = 'header__toggle';
 
-for (let i=0; i<3; i++){
+for (let i = 0; i < 3; i++) {
   toggleBtn.appendChild(document.createElement('span'));
 }
 
@@ -16,19 +16,20 @@ header.appendChild(nav);
 
 toggleBtn.addEventListener('click', () => {
   toggleBtn.classList.toggle('open');
-  nav.classList.toggle('open')});
+  nav.classList.toggle('open')
+});
 
+const headerMenu = document.createElement('ul');
+headerMenu.className = 'header__menu';
 
-  const headerMenu = document.createElement('ul');
-  headerMenu.className ='header__menu';
-
-  ['Play online', 'Play on-site', 'The story', 'Contact us'].forEach (text=> {const li =document.createElement('li');
-    li.className='header__item';
-    const a = document.createElement('a');
-    a.className = 'header__link';
-    a.href = '#';
-    a.textContent =text;
-    li.appendChild(a);
-    headerMenu.appendChild(li);
-  });
-  nav.appendChild(headerMenu);
+['Play online', 'Play on-site', 'The story', 'Contact us'].forEach(text => {
+  const li = document.createElement('li');
+  li.className = 'header__item';
+  const a = document.createElement('a');
+  a.className = 'header__link';
+  a.href = '#';
+  a.textContent = text;
+  li.appendChild(a);
+  headerMenu.appendChild(li);
+});
+nav.appendChild(headerMenu);
