3c7f4e8425d8cc53cf1f94130e8ee33956ac76d6
elinmolindr <elinmolinder1@gmail.com>
2025-11-06 11:20:37 +0100

    add classlist to header

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const toggleBtn = document.querySelector('.header__toggle');
 const nav = document.querySelector('.header__nav');
+const header = document.querySelector('.header');
 
 toggleBtn.addEventListener('click', () => {
   toggleBtn.classList.toggle('open');
   nav.classList.toggle('open');
+  header.classList.toggle('open');
 });
